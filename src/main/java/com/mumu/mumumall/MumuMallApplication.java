298dc798c8fc23b00e2df0d89295a5de414113fd
MuMu <piggy925@163.com>
2021-07-28 00:07:13 +0800

    feat:完成开发环境配置

---
+++ src/main/java/com/mumu/mumumall/MumuMallApplication.java
@@ -0,0 +1,13 @@
+package com.mumu.mumumall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumuMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumuMallApplication.class, args);
+    }
+
+}
